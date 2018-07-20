30e6eaef30128d88de5bdefbcad1b3f7366da0c7
oksanastarazhok <Oksana_Starazhok@epam.com>
2018-07-20 11:05:41 +0300

    changed url

--- src/test/java/GloboMainTestPage.java
+++ src/test/java/GloboMainTestPage.java
@@ -42,7 +42,7 @@ public class GloboMainTestPage {
 
         }
 
-        URL url = new URL("http://192.168.100.6:4444/wd/hub");
+        URL url = new URL("http://10.6.126.55:4444/wd/hub");
         WebDriver driver = new RemoteWebDriver(url, cap);
         driver.get("https://www.globoforce.com/");
         Assert.assertEquals(driver.getTitle(), "Globoforce | Elevate Workplace Culture and Performance | Globoforce");
